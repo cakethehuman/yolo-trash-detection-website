ee482266223dae81dc1a44305c66923a2336103b
Christian3322 <christiansusilo4@gmail.com>
2026-08-04 18:59:24 +0700

    feat(ui): add image preview container in the result section

--- frontend/script.js
+++ frontend/script.js
@@ -1,15 +1,62 @@
-async function connectBackend() {
-    try {
-        const response = await fetch("http://127.0.0.1:8000/");
-        const data = await response.json();
+document.addEventListener("DOMContentLoaded", () => {
+    const imageInput = document.getElementById("imageInput");
+    const previewImage = document.getElementById("previewImage");
+    const previewText = document.getElementById("previewText");
+    const detectBtn = document.getElementById("detectBtn");
+    
+    const resultImageContainer = document.getElementById("resultImageContainer");
+    const resultImage = document.getElementById("resultImage");
+    const predictionText = document.getElementById("prediction");
+    const confidenceText = document.getElementById("confidence");
+    const objectsText = document.getElementById("objects");
+
+    let uploadedImageUrl = null;
+
+    imageInput.addEventListener("change", function(event) {
+        const file = event.target.files[0];
         
-        document.getElementById("status").textContent =
-            `Terhubung ke backend: ${data}`;
+        if (file) {
+            const reader = new FileReader();
+            
+            reader.onload = function(e) {
+                uploadedImageUrl = e.target.result;
+                
+                previewImage.src = uploadedImageUrl;
+                previewImage.style.display = "block"; 
+                previewText.style.display = "none";   
+                
+                if (resultImageContainer) {
+                    resultImageContainer.style.display = "none";
+                }
+                
+                predictionText.textContent = "-";
+                confidenceText.textContent = "-";
+                objectsText.textContent = "-";
+            };
+            
+            reader.readAsDataURL(file);
+        } else {
+            previewImage.style.display = "none";
+            previewText.style.display = "block";
+            uploadedImageUrl = null;
+        }
+    });
 
-    } catch (error) {
-        document.getElementById("status").textContent =
-            "Gagal terhubung ke backend";
-    }
-}
 
-connectBackend();
+    // Data Dummy
+    detectBtn.addEventListener("click", () => {
+        if (!uploadedImageUrl) {
+            alert("Upload gambar!");
+            return;
+        }
+
+        if (resultImageContainer && resultImage) {
+            resultImage.src = uploadedImageUrl;
+            resultImageContainer.style.display = "flex"; 
+        }
+
+        predictionText.textContent = "Plastic Bottle";
+        confidenceText.textContent = "98.5%";
+        objectsText.textContent = "1";
+    });
+});
